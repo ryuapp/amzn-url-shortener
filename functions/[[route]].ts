b77cf773b58c1e27b9fdcf6fc511d31089447d12
ryu <114303361+ryuapp@users.noreply.github.com>
2023-05-19 22:26:09 +0900

    🩹 Add regex to remove last character

--- functions/[[route]].ts
+++ functions/[[route]].ts
@@ -8,7 +8,8 @@ app.get('/', (c) => {
 }).post('/', async (c) => {
   const { url } = await c.req.json<{ url: string }>()
   const domainRegex = /([a-zA-Z]{2,}):\/\/(smile.|www.|)amazon.(ae|au|ca|cn|de|es|eg|fr|in|it|jp|mx|nl|pr|sa|se|sg|co.jp|co.uk|com|com.be|com.br|com.mx|com.tr)/
-  const pathRegex = /\/dp\/\S*\//
+  const pathRegex = /\/dp\/\S*(\/|\?)/
+  const lastRegex = /(\/|\?)$/
   const domainName = url.match(domainRegex)
   const pathName = url.match(pathRegex)
   if (!domainName) {
@@ -21,7 +22,7 @@ app.get('/', (c) => {
       message: 'Can\'t shorten the url',
     })
   }
-  const shortenURL = domainName[0] + pathName[0]
+  const shortenURL = (domainName[0] + pathName[0]).replace(lastRegex, '')
   return c.json({ url: shortenURL })
 })
 
