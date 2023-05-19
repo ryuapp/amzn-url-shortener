2b24e14c3719842d30dec43ed53d44dc6e66eb81
ryu <114303361+ryuapp@users.noreply.github.com>
2023-05-19 20:09:48 +0900

    🎉 Initial commit

---
+++ functions/[[route]].ts
@@ -0,0 +1,28 @@
+import { Hono } from 'hono'
+import { handle } from 'hono/cloudflare-pages'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('🪓Amzn URL shortener')
+}).post('/', async (c) => {
+  const { url } = await c.req.json<{ url: string }>()
+  const domainRegex = /([a-zA-Z]{2,}):\/\/(smile.|www.|)amazon.(ae|au|ca|cn|de|es|eg|fr|in|it|jp|mx|nl|pr|sa|se|sg|co.jp|co.uk|com|com.be|com.br|com.mx|com.tr)/
+  const pathRegex = /\/dp\/\S*\//
+  const domainName = url.match(domainRegex)
+  const pathName = url.match(pathRegex)
+  if (!domainName) {
+    return c.json({
+      message: 'Invalid amazon url',
+    })
+  }
+  if (!pathName) {
+    return c.json({
+      message: 'Can\'t shorten the url',
+    })
+  }
+  const shortenURL = domainName[0] + pathName[0]
+  return c.json({ url: shortenURL })
+})
+
+export const onRequest = handle(app)
